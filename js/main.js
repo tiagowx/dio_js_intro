d0313891a9ce2bca6fc62bfafe170947c57d29e3
tiagowx <tiagobatista.tec@gmail.com>
2021-08-20 04:31:36 -0300

    Parte 2/5

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+var frutas = [{nome: "maçã", cor: "vermelha"},{nome: "uva", cor: "roxa"}]; // array de Objetos para
+console.log(frutas[1].nome);
+
+
+/* Objeto 
+var fruta = {nome: "maçã", cor: "vermelha"}; // Atribui um objeto JSON a variável
+console.log(fruta.nome+ " " + fruta.cor);
+*/
+
+
+/* // Tratamento de vetores(array)
+var lista = ["maçã", "pêra" , "laranja"]; // Atribui um array com valorea a variável lista
+
+lista.push("uva"); // Adciona/empurra o item 'uva' para a lista
+lista.pop(); // remove o ultimo item adicionado da lista
+
+console.log(lista); // retorna a lista
+console.log(lista.toString()); // retorna a lista como string
+console.log(lista.join(" - ")); // retorna a lista com separador customizado
+*/
+
+/* // Tratamento de variáveis
 var nome = "Tiago Batista";
 var idade = 29;
 var n1 = 5;
@@ -8,3 +30,4 @@ console.log(n1 * n2);
 console.log(frase.toLowerCase()); // transforma letras maiusculas em minusculas
 console.log(frase.toUpperCase()); // transforma letras minusculas em maiusculas
 console.log(frase.replace("Japão", "Brasil")); // substitui uma string ou caractere por outro 
+*/
