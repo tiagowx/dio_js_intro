030c0d0809534ff9fc66f06d6ecbf05f9c025ae1
tiagowx <tiagobatista.tec@gmail.com>
2021-08-20 03:53:49 -0300

    Parte 1/5

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var nome = "Tiago Batista";
+var idade = 29;
+var n1 = 5;
+var n2 = 3;
+var frase = "Japão é o melhor time do mundo";
+alert(nome + " tem " + idade + " anos")
+console.log(n1 * n2);
+console.log(frase.toLowerCase()); // transforma letras maiusculas em minusculas
+console.log(frase.toUpperCase()); // transforma letras minusculas em maiusculas
+console.log(frase.replace("Japão", "Brasil")); // substitui uma string ou caractere por outro 
